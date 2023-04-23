e53aa3bc9420441c1e3c9a2d5f53a65d27821ec0
Nestor Villafane <mass.nestor@gmail.com>
2023-04-22 21:29:48 -0500

    Se agreag menu

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart' show IconData, Icons;
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(title: 'Botones', subTitle: 'varios botones en flutter', link: '/buttons', icon: Icons.smart_button_outlined),
+  MenuItem(title: 'Tarjestas', subTitle: 'Un contenedor estilizado', link: '/tarjetas', icon: Icons.credit_card),
+];
